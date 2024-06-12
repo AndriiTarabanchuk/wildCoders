06811cf80a21d5e4fdb0e39f7b6ef95b9b404f61
DariusBlain <154019878+DariusBlain@users.noreply.github.com>
2024-06-13 01:25:13 +0200

    finish

--- src/js/header.js
+++ src/js/header.js
@@ -20,24 +20,76 @@ btn.addEventListener('click', () => {
 
 // =========================Switch-Themes===================================
 
-const btnThemes = document.querySelector('.span-themes');
-const btnCirckle = document.querySelector('.span-circkle');
-const btnMoon = document.querySelector('.span-moon');
-const section = document.querySelector('.header');
+const switcherWrapper = document.querySelector('.switcher-wrapper');
+const switcherMenu = document.querySelector('.switcher-menu');
 
 let blackThemes = false;
+const fragment = `<span class="span-themes"
+        ><span class="span-circkle"></span><span class="span-moon"></span
+      ></span>`;
 
-btnThemes.addEventListener('click', () => {
-  btnThemes.classList.toggle('span-active');
-  btnCirckle.classList.toggle('transform');
-  btnMoon.classList.toggle('transform-moon');
-  switcher();
-});
+document.addEventListener('DOMContentLoaded', screenWidth);
+window.addEventListener('resize', screenWidth);
 
-function switcher() {
-  if (btnThemes.classList.contains('span-active')) {
+function screenWidth() {
+  if (window.innerWidth <= 768) {
+    switcherWrapper.innerHTML = '';
+    switcherMenu.innerHTML = fragment;
+  } else {
+    switcherMenu.innerHTML = '';
+    switcherWrapper.innerHTML = fragment;
+    modal.classList.remove('mob-menu-active');
+    burger.classList.remove('active');
+  }
+  initDom();
+}
+
+function initDom() {
+  const btnThemes = document.querySelector('.span-themes');
+  const btnCirckle = document.querySelector('.span-circkle');
+  const btnMoon = document.querySelector('.span-moon');
+
+  if (btnThemes && btnCirckle && btnMoon) {
+    btnThemes.addEventListener('click', () => {
+      btnThemes.classList.toggle('span-active');
+      btnCirckle.classList.toggle('transform');
+      btnMoon.classList.toggle('transform-moon');
+
+      if (btnThemes.classList.contains('span-active')) {
+        document.body.setAttribute('dark', '');
+        blackThemes = true;
+      } else {
+        document.body.removeAttribute('dark', '');
+        blackThemes = false;
+      }
+      localStorage.setItem('blackThemes', blackThemes);
+    });
+    validation(btnThemes, btnCirckle, btnMoon);
+  }
+}
+
+function validation(btnThemes, btnCirckle, btnMoon) {
+  const isBlackThemes = localStorage.getItem('blackThemes') === 'true';
+
+  if (isBlackThemes) {
     document.body.setAttribute('dark', '');
+
+    if (btnThemes && btnCirckle && btnMoon) {
+      btnThemes.classList.add('span-active');
+      btnCirckle.classList.add('transform');
+      btnMoon.classList.add('transform-moon');
+    }
   } else {
     document.body.removeAttribute('dark', '');
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const isBlackThemes = localStorage.getItem('blackThemes') === 'true';
+  if (isBlackThemes) {
+    document.body.setAttribute('dark', '');
+  } else {
+    document.body.removeAttribute('dark', '');
+  }
+  screenWidth();
+});
